8ebeb15baeea62ca3f777a0d46080281a738a3a1
Melissa Rose <123melissa321@gmail.com>
2022-08-04 23:36:18 +0100

    Added cache counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { greet } from "./utils/greet";
+import {useState, useEffect} from 'react';
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [count, setCount] = useState<number>(() => {
+    let value;
+    try {
+      value = JSON.parse(window.localStorage.getItem('my-app-count') || '0');
+    }
+    catch (e) {
+      value = 0;
+    }
+    return value;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('my-app-count', JSON.stringify(count))
+  }, [count])
+
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </>
+  );
 }
 
 export default App;
