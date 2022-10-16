14e183a0b0af6e36daddd57860376f465acf0b51
Melissa Rose <123melissa321@gmail.com>
2022-10-16 20:48:35 +0100

    Pushed local edits before workspace deletion

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(() => {
     let value;
     try {
-      value = JSON.parse(window.localStorage.getItem('my-app-count') || '0');
-    }
-    catch (e) {
-      value = 0;
+      value = JSON.parse(window.localStorage.getItem("my-app-count") || "0");
+    } catch (e) {
+      value = false;
     }
     return value;
   });
 
   useEffect(() => {
-    window.localStorage.setItem('my-app-count', JSON.stringify(count))
-  }, [count])
+    window.localStorage.setItem("my-app-count", JSON.stringify(count));
+  }, [count]);
 
   return (
     <>
